7c9dfefddc6168fb9b354264ed10fe38cf84e752
Ansh Jaiswal <anshjaiswal1804@gmail.com>
2024-09-15 21:52:09 +0530

    new-subjects

--- RMS/main/script.js
+++ RMS/main/script.js
@@ -16,6 +16,22 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+    var popup = document.getElementById("popup");
+
+  var closeButton = document.getElementsByClassName("close")[0];
+
+  popup.style.display = "block";
+
+  closeButton.onclick = function() {
+    popup.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == popup) {
+      popup.style.display = "none";
+    }
+  }
+
     window.filterSubjects = function () {
         const filter = searchBox.value.toLowerCase();
         const subjects = subjectsList.getElementsByClassName('subject-item');
