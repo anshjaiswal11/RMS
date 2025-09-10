45ea43143fb2bfc3c36427e7894cb0c43ad552d8
Ansh Jaiswal <145288803+anshjaiswal11@users.noreply.github.com>
2025-09-10 16:17:56 +0530

    v17-v3

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'react-hot-toast';
 import './index.css';
 import App from './App';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
+// import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+// import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
